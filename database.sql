5a756a7cd8cccb1b690c0cdbbd36289fa9e3c6ed
gabistakk <gabistakk@gmail.com>
2023-04-17 09:42:03 -0300

    ATUALIZAÇÃO DATABASE

---
+++ database.sql
@@ -0,0 +1,95 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 17-Abr-2023 às 14:41
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `report_bullying`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `denuncia`
+--
+
+CREATE TABLE `denuncia` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(255) DEFAULT NULL,
+  `mensagem` varchar(255) DEFAULT NULL,
+  `isAnon` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `RA` varchar(255) DEFAULT NULL,
+  `fk_Usuario_id` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `isAdmin` tinyint(1) DEFAULT NULL,
+  `senha` varchar(255) DEFAULT NULL,
+  `id` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `RA` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `denuncia`
+--
+ALTER TABLE `denuncia`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_Denuncia_2` (`fk_Usuario_id`);
+
+--
+-- Índices para tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `denuncia`
+--
+ALTER TABLE `denuncia`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `denuncia`
+--
+ALTER TABLE `denuncia`
+  ADD CONSTRAINT `FK_Denuncia_2` FOREIGN KEY (`fk_Usuario_id`) REFERENCES `usuario` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
