32c74cb6fcc8ef0d5a8b3a89258582232f9c0135
caiobga99 <caiobga99@gmail.com>
2023-04-19 21:43:14 -0300

    chamando o style de login no header.

--- partials/header.php
+++ partials/header.php
@@ -8,8 +8,9 @@
     <link rel="stylesheet" href="../../css/style.css" />
     <link rel="stylesheet" href="../../screens/FaleConosco/styles.css" />
     <link rel="stylesheet" href="../../screens/Home/styles.css" />
+    <link rel="stylesheet" href="../../screens/Login/styles.css" />
     <!-- <link rel="stylesheet" href="reset.css"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous" />
-    <title>Denuncias</title>
+    <title>Report Bullying</title>
 </head>
