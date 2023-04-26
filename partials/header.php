ef0e1d9a8aa932aac0c04b781c1fcc08b11f2047
caiobga99 <caiobga99@gmail.com>
2023-04-26 05:39:05 -0300

    adicionando os arquivos de css'

--- partials/header.php
+++ partials/header.php
@@ -6,9 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="../../css/style.css" />
-    <link rel="stylesheet" href="../../screens/FaleConosco/styles.css" />
-    <link rel="stylesheet" href="../../screens/Home/styles.css" />
-    <link rel="stylesheet" href="../../screens/Login/styles.css" />
+    <link rel="stylesheet" href="../../screens/FaleConosco/style.css" />
+    <link rel="stylesheet" href="../../screens/Home/style.css" />
+    <link rel="stylesheet" href="../../screens/Login/style.css" />
+    <link rel="stylesheet" href="../../screens/Cadastro/style.css" />
+    <link rel="stylesheet" href="../assets/colors/colors.css" />
+    <link rel="stylesheet" href="../assets/fonts/fonts.css" />
     <!-- <link rel="stylesheet" href="reset.css"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous" />
