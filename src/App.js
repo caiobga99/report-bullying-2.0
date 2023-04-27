eca4c3f4a2afc63cad907d91a141b724a1d74d29
caiobga99 <caiobga99@gmail.com>
2023-04-27 12:12:02 -0300

    adicionando arquivo de rotas.

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import Home from "./screens/Home/index";
+import Cadastro from "./screens/Cadastro/index";
+import Login from "./screens/Login/index";
+import FaleConosco from "./screens/FaleConosco/index";
+import NotFound from "./screens/NotFound/index";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<Home />} />
+        <Route path="/fale-conosco" element={<FaleConosco />} />
+        <Route path="/cadastrar" element={<Cadastro />} />
+        <Route path="/entrar" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
