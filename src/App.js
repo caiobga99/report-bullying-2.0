8bb4eb1e56645a9f66f8558f12f7f08d476b0859
caiobga99 <caiobga99@gmail.com>
2023-05-03 19:24:43 -0300

    adicionando rota do faq.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./screens/Home/index";
 import Cadastro from "./screens/Cadastro/index";
 import Login from "./screens/Login/index";
 import FaleConosco from "./screens/FaleConosco/index";
+import Faq from "./screens/Faq/index";
 import NotFound from "./screens/NotFound/index";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/fale-conosco" element={<FaleConosco />} />
         <Route path="/cadastrar" element={<Cadastro />} />
         <Route path="/entrar" element={<Login />} />
+        <Route path="/faq" element={<Faq />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
