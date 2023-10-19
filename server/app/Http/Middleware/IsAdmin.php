c64a387006f19d6630213af9f2cb618eff33c4ec
caiobga99 <caiobga99@gmail.com>
2023-10-19 08:04:11 -0300

    adicionando verificacao do tipo de usuario.

---
+++ server/app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Auth;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::user() && Auth::user()->tipo_usuario == 1) {
+            return $next($request);
+        }
+
+        return redirect('/');
+    }
+}
