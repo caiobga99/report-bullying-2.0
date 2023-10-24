11d0be6d5d73878f05103555174c81c1225bfe9b
caiobga99 <caiobga99@gmail.com>
2023-10-24 10:33:23 -0300

    adicionando context do token para pegar do server.

---
+++ mobile/common/Token.js
@@ -0,0 +1,19 @@
+import { createContext, useContext, useState } from "react";
+
+export const TokenContext = createContext();
+
+export const TokenProvider = ({ children }) => {
+  const [token, setToken] = useState("");
+  console.log(token);
+  return (
+    <TokenContext.Provider value={{ token, setToken }}>
+      {children}
+    </TokenContext.Provider>
+  );
+};
+
+export const useToken = () => {
+  const token = useContext(TokenContext);
+  return token;
+};
+export default TokenProvider;
