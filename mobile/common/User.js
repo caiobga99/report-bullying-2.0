944dbabc9e60fed626c7900b76214da347fed7ab
caiobga99 <caiobga99@gmail.com>
2023-10-25 08:47:36 -0300

    adicionando context do usuario para ver se esta logado.

---
+++ mobile/common/User.js
@@ -0,0 +1,19 @@
+import { createContext, useContext, useState } from "react";
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [isLogged, setIsLogged] = useState(false);
+  console.log(isLogged);
+  return (
+    <UserContext.Provider value={{ isLogged, setIsLogged }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => {
+  const user = useContext(UserContext);
+  return user;
+};
+export default useUser;
