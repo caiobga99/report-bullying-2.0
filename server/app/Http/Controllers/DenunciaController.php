87292e2ba1cc1630f51c9130e2172d628508ccbc
caiobga99 <caiobga99@gmail.com>
2023-10-26 09:23:34 -0300

    atualizando.

--- server/app/Http/Controllers/DenunciaController.php
+++ server/app/Http/Controllers/DenunciaController.php
@@ -54,7 +54,7 @@ class DenunciaController extends Controller
     public function show()
     {
         $id = Auth::id();
-        $denuncia = Denuncia::all()->where('id_usuario', $id)->values(); //problema no map das denuncias aqui por causa do indice retornado
+        $denuncia = Denuncia::all()->where('id_usuario', $id)->values();
         return $denuncia;
     }
 
