02ed08d26f667dac256eed287f5ad09d24fe93cb
caiobga99 <caiobga99@gmail.com>
2023-11-17 09:50:38 -0300

    adicionando variavel global do usuario.

---
+++ client/src/common/User.tsx
@@ -0,0 +1,46 @@
+import { createContext, useContext, useState } from "react";
+
+type UserContextType = {
+  isLogged: boolean;
+  setIsLogged: (value: boolean) => void;
+  viewReport: boolean;
+  setViewReport: (value: boolean) => void;
+  isAnonymous: boolean;
+  setIsAnonymous: (value: boolean) => void;
+  isAdmin: boolean;
+  setIsAdmin: (value: boolean) => void;
+};
+
+export const UserContext = createContext<UserContextType | undefined>(
+  undefined
+);
+
+export const UserProvider = ({ children }: any) => {
+  const [isLogged, setIsLogged] = useState(false);
+  const [viewReport, setViewReport] = useState(false);
+  const [isAnonymous, setIsAnonymous] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
+  console.log(isAdmin);
+  return (
+    <UserContext.Provider
+      value={{
+        isLogged,
+        setIsLogged,
+        viewReport,
+        setViewReport,
+        isAnonymous,
+        setIsAnonymous,
+        isAdmin,
+        setIsAdmin,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => {
+  const user = useContext(UserContext);
+  return user;
+};
+export default useUser;
