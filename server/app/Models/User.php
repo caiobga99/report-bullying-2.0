72c60795a74b91a3644d7389d8080c95c3800028
caiobga99 <caiobga99@gmail.com>
2023-11-17 12:33:47 -0300

    atualizando.

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-
 class User extends Authenticatable
 {
     use HasUuids;
