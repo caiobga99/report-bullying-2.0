276e6aaf359443841afecb58a30fb5a4b0871009
caiobga99 <caiobga99@gmail.com>
2023-11-21 12:26:46 -0300

    criando card da denuncia.

---
+++ client/src/components/ReportCard/index.tsx
@@ -0,0 +1,58 @@
+const ReportCard = () => {
+  return (
+    <div className="max-w-md p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-end">
+      <a
+        href="#"
+        className="inline-flex items-center text-blue-600 hover:underline"
+      >
+        Visualizar a resposta da denuncia
+        <svg
+          className="w-3 h-3 ms-2.5 rtl:rotate-[270deg]"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 18 18"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"
+          />
+        </svg>
+      </a>
+      <a href="#">
+        <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+          Need a help in Claim? jn asdasd buasbfsbab
+        </h5>
+      </a>
+      <textarea
+        rows={6}
+        disabled
+        className="mb-3 font-normal text-gray-500 dark:text-gray-400 min-w-full"
+      >
+        Go to this step by step guideline process on how to certify for your
+        weekly benefits: Go to this step by step guideline process on how to
+        certify for your weekly benefits: Go to this step by step guideline
+        process on how to certify for your weekly benefits: Go to this step by
+        step guideline process on how to certify for your weekly benefits: Go to
+        this step by step guideline process on how to certify for your weekly
+        benefits: Go to this step by step guideline process on how to certify
+        for your weekly benefits: Go to this step by step guideline process on
+        how to certify for your weekly benefits: Go to this step by step
+        guideline process on how to certify for your weekly benefits: Go to this
+        step by step guideline process on how to certify for your weekly
+        benefits: Go to this step by step guideline process on how to certify
+        for your weekly benefits: Go to this step by step guideline process on
+        how to certify for your weekly benefits: Go to this step by step
+        guideline process on how to certify for your weekly benefits: Go to this
+        step by step guideline process on how to certify for your weekly
+        benefits: Go to this step by step guideline process on how to certify
+        for your weekly benefits:
+      </textarea>
+    </div>
+  );
+};
+
+export default ReportCard;
