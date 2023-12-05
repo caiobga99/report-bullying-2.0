a6dc3ef42fc41d4176859daa988b18874340b3b9
caiobga99 <caiobga99@gmail.com>
2023-12-05 10:27:54 -0300

    adicionando arquivo de export de interfaces.

---
+++ client/src/utils/protocols.ts
@@ -0,0 +1,11 @@
+export interface Denuncias {
+  RA: string;
+  created_at: string;
+  email: string;
+  id_denuncia: string;
+  id_usuario: string;
+  mensagem: string;
+  nome: string;
+  titulo: string;
+  updated_at: string;
+}
