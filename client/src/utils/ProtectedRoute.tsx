e8116ec99803757b64ac95fd7c5e42f2639c3760
caiobga99 <caiobga99@gmail.com>
2023-12-04 22:26:28 -0300

    criando protetor de rotas.

---
+++ client/src/utils/ProtectedRoute.tsx
@@ -0,0 +1,17 @@
+import { Navigate } from "react-router";
+import showToastMessage from "./showToastMessage";
+
+interface ProtectedRoutesProps {
+    token: string | null;
+    children: JSX.Element;
+}
+
+const ProtectedRoute = ({ token, children }: ProtectedRoutesProps) => {
+    if (!token) {
+        showToastMessage("Voce precisa estar logado para acessar essa rota!", "error");
+        return <Navigate to={"/"} replace />;
+
+    }
+    return children;
+}
+export default ProtectedRoute;
