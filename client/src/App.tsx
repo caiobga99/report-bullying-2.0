5af2692e49dbadac87d6745197eba8927cf2f0ee
caiobga99 <caiobga99@gmail.com>
2023-12-07 07:35:59 -0300

    adicionando vlibras.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { useTema } from "./common/Tema";
 import Navbar from "./components/Navbar";
 import MyRoutes from "./routes";
+import VLibras from "@moreiraste/react-vlibras";
 function App() {
   const { pegarTema } = useTema() as {
     setPegarTema: (value: string) => void;
@@ -8,6 +9,7 @@ function App() {
   };
   return (
     <div>
+      <VLibras forceOnload={true} />
       <Navbar />
       <div className={pegarTema === "dark" ? "bg-white" : "bg-white"}>
         <MyRoutes />
