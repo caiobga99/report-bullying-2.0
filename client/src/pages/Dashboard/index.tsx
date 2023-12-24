f0267829c06bb2a2ca2999521a6b928ab69824b0
caiobga99 <caiobga99@gmail.com>
2023-12-24 16:17:50 -0300

    adicionando card de estatisticas.

--- client/src/pages/Dashboard/index.tsx
+++ client/src/pages/Dashboard/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from "react";
 import api from "../../lib/api";
-import { User } from "../../utils/protocols";
+import { Denuncias, User } from "../../utils/protocols";
 import { Spinner } from "flowbite-react";
 import { Dropdown, Alert, Tooltip } from "flowbite-react";
 import {
@@ -21,11 +21,14 @@ import {
 } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
 import { format } from "date-fns";
+import showToastMessage from "../../utils/showToastMessage";
 const Dashboard = () => {
   const [users, setUsers] = useState<User[] | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [denunciasIsLoading, setDenunciasIsLoading] = useState<boolean>(true);
   const [show, setShow] = useState<boolean>(true);
   const [search, setSearch] = useState<string>("");
+  const [denuncias, setDenuncias] = useState<Denuncias[]>([]);
   const [searchType, setSearchType] = useState<
     "email" | "RA" | "id_usuario" | "nome"
   >("email");
@@ -37,6 +40,16 @@ const Dashboard = () => {
         setUsers(res.data);
       })
       .catch((err) => console.log(err.message));
+    api
+      .get("/denuncias")
+      .then((res) => {
+        setDenuncias(res.data);
+        setDenunciasIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err.message);
+        showToastMessage(err.message, "error");
+      });
   }, []);
   const filteredUsers = useMemo(() => {
     const check = users?.filter((user) =>
@@ -53,6 +66,61 @@ const Dashboard = () => {
         </div>
       ) : (
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg min-w-[80%] min-h-full">
+          <div className="flex gap-5">
+            <div className="min-w-0 rounded-lg shadow-xs overflow-hidden bg-white dark:bg-gray-800">
+              <div className="p-4 flex items-center">
+                <div className="p-3 rounded-full text-orange-500 dark:text-orange-100 bg-orange-100 dark:bg-orange-500 mr-4">
+                  <svg
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    className="w-5 h-5"
+                  >
+                    <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"></path>
+                  </svg>
+                </div>
+                <div>
+                  <p className="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
+                    Total de usuarios
+                  </p>
+                  <p className="text-lg font-semibold text-gray-700 dark:text-gray-200">
+                    {users?.length}
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="min-w-0 rounded-lg shadow-xs overflow-hidden bg-white dark:bg-gray-800">
+              <div className="p-4 flex items-center">
+                <div className="p-3 rounded-full text-teal-500 dark:text-teal-100 bg-teal-100 dark:bg-teal-500 mr-4">
+                  <svg
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    className="w-5 h-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                </div>
+                <div>
+                  <p className="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
+                    Total de denuncias
+                  </p>
+                  {denunciasIsLoading ? (
+                    <div className="flex items-center justify-center pr-5">
+                      <Spinner />
+                    </div>
+                  ) : (
+                    <p className="text-lg font-semibold text-gray-700 dark:text-gray-200">
+                      {denuncias.length}
+                    </p>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="flex items-center justify-between flex-column md:flex-row flex-wrap space-y-4 md:space-y-0 py-4 bg-light dark:bg-gray-900">
             <div className="flex gap-4 items-center">
               <Dropdown
@@ -596,6 +664,7 @@ const Dashboard = () => {
                   })}
             </tbody>
           </table>
+          
         </div>
       )}
     </div>
