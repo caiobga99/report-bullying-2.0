1448ee3f2a257dcb9f53f22248f9f3ad121136ca
caiobga99 <caiobga99@gmail.com>
2023-12-26 23:51:02 -0300

    adicionando migration do comentario.

---
+++ server/database/migrations/2023_12_27_023137_create_comentarios_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Denuncia;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comentarios', function (Blueprint $table) {
+            $table->uuid("id_comentario")->primary();
+            $table->longText("mensagem");
+            $table->foreignIdFor(User::class, "id_usuario")->nullable();
+            $table->foreignIdFor(Denuncia::class, "id_denuncia");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comentarios');
+    }
+};
