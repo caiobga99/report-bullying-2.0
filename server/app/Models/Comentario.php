b1f41e5c68017c42bdfc2e1a587eb287cb5221e2
caiobga99 <caiobga99@gmail.com>
2023-12-26 23:51:18 -0300

    adicionando model do comentario.

---
+++ server/app/Models/Comentario.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comentario extends Model
+{
+    use HasUuids;
+    use HasFactory;
+
+    protected $table = "comentarios";
+    protected $primaryKey = "id_comentario";
+    protected $fillable = ["id_comentario", "mensagem", "id_usuario", "id_denuncia"];
+
+    public function usuarios()
+    {
+        return $this->hasMany(User::class, "id_usuario");
+    }
+    public function denuncias()
+    {
+        return $this->hasMany(Denuncia::class, "id_usuario");
+    }
+}
